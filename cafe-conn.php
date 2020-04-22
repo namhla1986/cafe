3f4a044702b7d597eb3105355188d71ac5741efb
Namhla <mankune@yahoo.com>
2020-04-22 15:07:40 +0800

    seventh commit

--- cafe-conn.php
+++ cafe-conn.php
@@ -1,13 +1,14 @@
+ggg
 <?php
     $customerName = $_POST['customerName'];
     $customerEmail = $_POST['customerEmail'];
     $customerMessage = $_POST['customerMessage'];
     //Database connection
-    $conn = new mysqli('localhost', 'root','','duncans');
+    $conn = new mysqli('localhost', 'root','3306','democafe');
     if($conn->connect_error){
         die('Connection Failed : '. $conn->connect_error);
     }else{
-        $stmt = $conn->prepare("insert into messages(customerName, customerEmail, customerMessage) values(?, ?, ?)");
+        $stmt = $conn->prepare("insert into enquiries(customerName, customerEmail, customerMessage) values(?, ?, ?)");
         $stmt->bind_param("sss", $customerName, $customerEmail, $customerMessage);
         $stmt->execute();
         echo("Message sent successfully..");
